053d48a69287709b6c7503707eadb98447ffe309
seoingoo <disk012@naver.com>
2015-12-26 22:30:24 +0900

    mailgun with github address

--- mailgun/mailgunTest.py
+++ mailgun/mailgunTest.py
@@ -5,9 +5,9 @@ def send_email() :
 		"https://api.mailgun.net/v3/sandboxd13de7857b744ce1872cea0b767eeb12.mailgun.org/messages",
 		auth=("api", "key-3d3d6b97bec40a2d401864e20179a949"),
 		data={"from": "mailgun test <mailgun@test>",
-		"to": ["disk012@ajou.ac.kr"],
-		"subject": "[test mail] Mail Gun Test",
-		"text": "Seoingoo, Mail Gun Last  Test\n"})
+		"to": ["gadin.kang@gmail.com"],
+		"subject": "[test mailgun] Mail Gun Test With Github Address",
+		"text": "Seoingoo, Mail Gun Last  Test\n\n  https://github.com/seoingoo/project"})
 
 if __name__ == "__main__" :
 	send_email()
